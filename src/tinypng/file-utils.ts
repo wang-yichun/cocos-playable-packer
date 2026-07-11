72b2d8e424638899fa767a2b1139e55b747a67e3
Ethan <yishionwang314@163.com>
2026-07-11 23:22:14 +0800

    fix: bridge build cache file utilities

---
+++ src/tinypng/file-utils.ts
@@ -0,0 +1,9 @@
+export {
+    isNodeError,
+    isRecord,
+    readJsonUnknown,
+    resolvePortableRelativePath,
+    toErrorMessage,
+    writeBufferAtomically,
+    writeJsonAtomically,
+} from "../tinypng-build/file-utils.js";
